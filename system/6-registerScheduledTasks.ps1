177e93cde790377fd73cbc67896ef878f2957a0a
Adde Lovein <adde@lovein.se>
2024-02-11 21:25:03 +0100

    final touches

--- system/6-registerScheduledTasks.ps1
+++ system/6-registerScheduledTasks.ps1
@@ -11,12 +11,12 @@ Write-Host "Create Task Job Eject-Config" -F Blue
 $taskName="Main Script"
 Write-Host "Register Job $taskName"
 $trigger=New-JobTrigger -AtStartup
-$action=New-ScheduledTaskAction -Execute PowerShell.exe -Argument "-ExecutionPolicy Bypass -file C:\cloud-init\system\cloud-init.system.ALWAYS.ps1"
+$action=New-ScheduledTaskAction -Execute PowerShell.exe -Argument "-ExecutionPolicy Bypass -WindowStyle hidden -file C:\cloud-init\system\cloud-init.system.ALWAYS.ps1"
 Register-ScheduledTask -Action $action -TaskName $taskName -Description "Proxmox Cloud-Init" -RunLevel Highest -Trigger $trigger -User "SYSTEM" -TaskPath Proxmox-cloud-init
 
 $taskName="User Script"
 Write-Host "Register Job $taskName"
-$action=New-ScheduledTaskAction -Execute PowerShell.exe -Argument "-ExecutionPolicy Bypass -file C:\cloud-init\user\cloud-init.system.ALWAYS.ps1"
+$action=New-ScheduledTaskAction -Execute PowerShell.exe -Argument "-ExecutionPolicy Bypass -WindowStyle hidden -file C:\cloud-init\user\cloud-init.user.ALWAYS.ps1"
 $princ = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrators" -RunLevel Highest
 $trigger=New-JobTrigger -AtLogon
 Register-ScheduledTask -Action $action -Principal $princ -Trigger $trigger -TaskName $taskName 
