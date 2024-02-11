3b840f5736c4cd533cc4b0e451d45b669d7c800a
Adde Lovein <adde@lovein.se>
2024-02-11 14:54:15 +0100

    Fix

--- INSTALL.ps1
+++ INSTALL.ps1
@@ -1,5 +1,8 @@
 Write-Host "Install Proxmox-cloud-init" -F Blue
+$ORIG=$pwd.path
+
 Get-PackageProvider -name nuget -force
 Install-Module -Name powershell-yaml -confirm:$false -Force
-cd \cloud-init\
-C:\Windows\System32\sysprep\sysprep.exe /generalize /oobe /unattend:Unattend.xml
+cd c:\cloud-init\
+C:\Windows\System32\sysprep\sysprep.exe /generalize /oobe /unattend:c:\cloud-init\Unattend.xml
+cd $ORIG
